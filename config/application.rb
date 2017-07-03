14f8a5f2c73669c0013698e09a7971b79235bb30
DESKTOP-6HD1PI1\Cess <pvflorendo@up.edu.ph>
2017-07-03 20:39:00 +0800

    Added front-end to admin log-in

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Corkboard
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    
+  
     ActionMailer::Base.smtp_settings = {
     
       :address      => 'smtp.gmail.com',
