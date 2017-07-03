9049b92077c234b24a51765d7e642e207d6ebb94
enkidoodles <jennieablog@gmail.com>
2017-07-03 10:36:12 +0800

    removed lines added by git

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+
   devise_for :users, controllers: { registrations: "registrations" }
-  resources :builds, path_names: {
-    show: ''
-  }
-=======
+
   resources :builds, except: [:index]
->>>>>>> 5310b260f54ce11a0806f638e4f124be5021b229
   resources :branches do
     resources :builds
   end
