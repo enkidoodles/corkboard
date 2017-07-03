4d937ea5bb78877052799591039ccdd98baff44c
Daniel <dlhizon@up.edu.ph>
2017-07-03 03:31:11 +0000

    Separate Login

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,7 @@ class PagesController < ApplicationController
     if user_signed_in?
       redirect_to branches_path
     else
-      redirect_to new_user_session_path
+      redirect_to root_path
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   devise_for :users, controllers: { registrations: "registrations" }
+  
+  get 'pages/home'
+  root 'pages#home'
+  
   resources :builds, path_names: {
     show: ''
   }
-=======
   resources :builds, except: [:index]
->>>>>>> 5310b260f54ce11a0806f638e4f124be5021b229
   resources :branches do
     resources :builds
   end
-  get 'pages/home'
-  root 'pages#home'
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
